39dec79ab8f352e329d3da85696b6bc191e0193f
Saif Al-Jilani <s.aljilani@futeric.com>
2023-10-10 14:26:51 +0300

    update services

--- src/app/emp-add-edit/emp-add-edit.component.ts
+++ src/app/emp-add-edit/emp-add-edit.component.ts
@@ -24,7 +24,7 @@ export class EmpAddEditComponent implements OnInit {
     private _empService: EmployeeService,
     private _dialogRef: MatDialogRef<EmpAddEditComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any,
-    private _coreService:MainService
+    private _mainService:MainService
   ) {
     this.empForm = this._fb.group({
       firstName: '',
@@ -50,7 +50,7 @@ export class EmpAddEditComponent implements OnInit {
           .updateEmployee(this.data.id, this.empForm.value)
           .subscribe({
             next: (val: any) => {
-              this._coreService.openSnackBar('Employee detail updated')
+              this._mainService.openSnackBar('Employee detail updated')
               this._dialogRef.close(true);
             },
             error: (err: any) => {
@@ -60,7 +60,7 @@ export class EmpAddEditComponent implements OnInit {
       } else {
         this._empService.addEmployee(this.empForm.value).subscribe({
           next: (val: any) => {
-            this._coreService.openSnackBar('Employee added successfully')
+            this._mainService.openSnackBar('Employee added successfully')
             this._dialogRef.close(true);
           },
           error: (err: any) => {
